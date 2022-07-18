8b9199ec6b514a4796abfff57f40a24ef2f38c07
Raphael Berger <raphaelmb@gmail.com>
2022-07-18 14:59:29 -0300

    Add Customer factory

---
+++ src/domain/customer/factory/customer.factory.ts
@@ -0,0 +1,14 @@
+import Customer from "../entity/customer";
+import { randomUUID } from "node:crypto";
+import Address from "../value-object/address";
+
+export default class CustomerFactory {
+  public static create(name: string): Customer {
+    return new Customer(randomUUID(), name);
+  }
+  public static createWithAddress(name: string, address: Address): Customer {
+    const customer = new Customer(randomUUID(), name);
+    customer.changeAddress(address);
+    return customer;
+  }
+}
